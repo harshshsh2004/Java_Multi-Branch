a01ab6bcd668ee8bc25b77f557f3b30a3169f335
harshshsh2004 <harshgotu2004@github.com>
2025-12-19 06:16:26 +0000

    Main branch dockerized

---
+++ src/main/java/com/demo/App.java
@@ -0,0 +1,8 @@
+package com.demo;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("🚀 PRODUCTION CONTAINER");
+        System.out.println("Branch: MAIN");
+    }
+}
