db0e9e923ce768240e91ffc060386f33085ce9ec
harshshsh2004 <harshgotu2004@github.com>
2025-12-19 06:39:15 +0000

    change app file

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -3,6 +3,6 @@ package com.demo;
 public class App {
     public static void main(String[] args) {
         System.out.println("🚀 PRODUCTION CONTAINER");
-        System.out.println("Branch: MAIN");
+        System.out.println("Branch: TEST");
     }
 }
